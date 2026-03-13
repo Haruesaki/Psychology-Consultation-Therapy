2976946a103a852ba7b7a9676e081a09bc68d9ef
Haruesaki <ahmaddonijalaludin@gmail.com>
2026-03-13 22:16:33 +0700

    Penambahan Button dan Crop Image

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.test
 
+import android.hardware.camera2.params.BlackLevelPattern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,13 +16,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.test.Model.Psikolog
@@ -33,67 +42,77 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        //name = "Android",
-                        //modifier = Modifier.padding(innerPadding)
-                    )
+                    ListPsikolog(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Selamat ulang tahun $name!",
-        modifier = modifier
-    )
-}
-
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Selamat ulang tahun $name!",
+//        modifier = modifier
+//    )
+//}
+//
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestTheme {
-        Greeting("Doni")
+        ListPsikolog()
     }
 }
 
 @Composable
-fun PsikologItem(psikolog: Psikolog){
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-    ) {
-        Image(
-            painter = painterResource(id = psikolog.imageRes),
-            contentDescription = "",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .padding(top = 8.dp)
-        )
-        HorizontalDivider(modifier = Modifier.padding(top = 16.dp))
-        Text(text = "Nama: ${psikolog.nama}", style = MaterialTheme.typography.bodyLarge)
-        Text(text = "Deskripsi ${psikolog.deskripsi}")
-        Text(text = "Harga: ${psikolog.harga}")
-
-    }
-}
-
-
-@Composable
-fun Greeting() {
+fun ListPsikolog(modifier: Modifier = Modifier) {
     val profil = PsikologSource.dataDiri
     LazyColumn(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)) {
         items(profil){psikolog ->
-            PsikologItem(psikolog = psikolog)
+            DetailScreen(psikolog = psikolog)
+        }
     }
+}
+
+@Composable
+fun DetailScreen(psikolog: Psikolog){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            painter = painterResource(id = psikolog.imageRes),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = psikolog.nama,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = psikolog.deskripsi,
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Pesan Sekarang")
+        }
     }
 }
